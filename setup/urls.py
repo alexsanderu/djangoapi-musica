aa5059ff54e6fe09a7375783766ac612bbffdea0
Alex Sander <sralexsadder@gmail.com>
2024-01-19 18:00:27 -0300

    ajuste

--- setup/urls.py
+++ setup/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('artistas/<int:pk>/albuns/', ListaAlbunsArtista.as_view()),
     path('artistas/<int:pk>/albuns/<int:album_id>/musicas/', MusicasDoAlbum.as_view()),
-    path('artistas/<int:pk>/albuns/musicas/<int:musica_id>', ListaMusicaAlbuns.as_view())
+    path('artistas/<int:pk>/albuns/musicas/<int:musica_id>', ListaMusicaAlbuns.as_view()),
+    path('albuns/<int:album_id>/musicas/', MusicasDoAlbum.as_view()),
 ]
